e0fd190603bb22d719252a4a22335da545948dac
Vanessa <ladyvane1412@gmail.com>
2023-08-22 16:46:26 -0500

    Actualizacion backend

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 
 //Settings
+app.set('port', process.env.PORT || 3000);
 
 //Middlewares
-
+app.use(express.json());
 //Routes
+app.use(require('./routes/users'));
+//Starting the server
 
-//Starling the server
-
-app.listen(3000, () => {
-    console.log('Server on port 3000')
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
 });
